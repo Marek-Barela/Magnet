955b5ce5fc5526fdbb19a2226ddbcf7f293b45c2
Marek Bareła <marek.barela@wp.pl>
2018-05-18 21:04:15 +0200

    small change

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,22 +1,24 @@
-const $hamburgerMenuButton = $('.navigation__hamburger'),
-    $navigationHiddenLayer = $('.navigation__hidden-menu');
+$(document).ready(function() {
+    const $hamburgerMenuButton = $('.navigation__hamburger'),
+        $navigationHiddenLayer = $('.navigation__hidden-menu');
 
-//Toggle betwen hidden and visible navigation
-function toggleHiddenMenu() {
-    $navigationHiddenLayer.toggleClass('navigation__hidden-menu--is-active')
-    $hamburgerMenuButton.toggleClass('navigation__hamburger--is-active')
-}
-
-//show border under navigation after scroll
-function borderUnderNavigation() {
-    if($(window).scrollTop()) {
-        $('.navigation').css('border-bottom', '3px solid black')
+    //Toggle betwen hidden and visible navigation
+    function toggleHiddenMenu() {
+        $navigationHiddenLayer.toggleClass('navigation__hidden-menu--is-active')
+        $hamburgerMenuButton.toggleClass('navigation__hamburger--is-active')
     }
-    else {
-        $('.navigation').css('border-bottom', 'none')
-    }
-}
 
-$hamburgerMenuButton.on('click', toggleHiddenMenu)
-$(window).on('scroll', borderUnderNavigation)
-borderUnderNavigation()
+    //show border under navigation after scroll
+    function borderUnderNavigation() {
+        if($(window).scrollTop()) {
+            $('.navigation').css('border-bottom', '3px solid black')
+        }
+        else {
+            $('.navigation').css('border-bottom', 'none')
+        }
+    }
+
+    $hamburgerMenuButton.on('click', toggleHiddenMenu)
+    $(window).on('scroll', borderUnderNavigation)
+    borderUnderNavigation()
+});
